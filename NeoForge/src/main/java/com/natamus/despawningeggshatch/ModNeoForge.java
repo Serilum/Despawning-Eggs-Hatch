971715be83750c09b49502b024701c8c7a415411
Rick South <rick@natamus.com>
2024-07-28 07:37:03 +0200

    (v4.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/despawningeggshatch/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/despawningeggshatch/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.despawningeggshatch;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.despawningeggshatch.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.despawningeggshatch.neoforge.events.NeoForgeEggEvent;
 import com.natamus.despawningeggshatch.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
