69ea114c2f9264692dc2f7d49ccf2ee14fcabc35
Rick South <rick@natamus.com>
2024-10-24 20:05:19 +0200

    Despawning Eggs Hatch source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/despawningeggshatch/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.despawningeggshatch.util;
+
+public class Reference {
+	public static final String MOD_ID = "despawningeggshatch";
+	public static final String NAME = "Despawning Eggs Hatch";
+	public static final String VERSION = "4.4";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
---
+++ Fabric/src/main/java/com/natamus/despawningeggshatch/ModFabric.java
@@ -0,0 +1,37 @@
+package com.natamus.despawningeggshatch;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.collective.fabric.callbacks.CollectiveItemEvents;
+import com.natamus.despawningeggshatch.events.EggEvent;
+import com.natamus.despawningeggshatch.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveItemEvents.ON_ITEM_EXPIRE.register((ItemEntity itemEntity, ItemStack itemStack) -> {
+			EggEvent.onItemExpire(itemEntity, itemStack);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
